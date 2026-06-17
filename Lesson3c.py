8bfbed8730a6e5ea56645f9dd7ff32a6a7174f34
Kimani-Kaminju <lansonkimani@gmail.com>
2026-06-17 14:45:41 +0300

    Added

---
+++ Lesson3c.py
@@ -0,0 +1,34 @@
+#Python loop
+#loop-control structure allows a block of code to be executed repeatedly untill the condition is met
+# types of loops:
+    #for loop
+    #while loop
+
+#python loop
+
+"for variable in range(n):"
+"   block of code to be executed"
+
+
+for greeting in range(500):
+    print("Hello there ",greeting)
+
+print("================")
+for number in range(10,20):
+    print(number)
+
+
+print("==================================")
+#below we use the following steps
+for x in range(50, 71, 2):
+    print(x)
+
+print("=================================")
+#program able to print all the odd numbers between 0 and 51
+
+for y in range (1,51,2):
+    print(y)
+
+
+
+
---
+++ Lesson3e.py
@@ -0,0 +1,24 @@
+#while loop-executes code until condition is met 
+#structure
+
+"initializing a variable with a value"
+"while keywords follow"
+"condition to be checked"
+"statements to be printed"
+"lasttly increment/decreement"
+
+number = 0
+while number <= 10:
+    print(number)
+    number = number + 1
+
+print("============================")
+#below we print from a higher value to lower value
+
+y=50
+
+while y >=0:
+    print(y)
+    y = y - 1
+
+# By use of a for loop, find all the leap years in between the year 2000 and 2030
